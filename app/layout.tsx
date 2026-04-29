ff62ea0396d2bbd13d637b8ac9679442767b23d3
kaushik0010 <paykolikaushikb@gmail.com>
2026-04-29 20:17:23 +0530

    feat: add Open Graph and Twitter metadata for rich link previews

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,32 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Life OS",
-  description: "Your personal Life OS",
+  description:
+    "Turn your memories into folders and files inside nostalgic devices like Windows XP and Nokia.",
+
+  openGraph: {
+    title: "Life OS",
+    description:
+      "Store your memories as folders and files — inside Windows XP and Nokia.",
+    url: "https://life-os-beta-ochre.vercel.app",
+    siteName: "Life OS",
+    images: [
+      {
+        url: "https://life-os-beta-ochre.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Your life... inside old devices",
+    description:
+      "Relive your memories as folders and files in a nostalgic OS experience.",
+    images: ["https://life-os-beta-ochre.vercel.app/og-image.png"],
+  },
 };
 
 export default function RootLayout({
